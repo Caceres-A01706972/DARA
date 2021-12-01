f55d04a762cff1c0b6a37b8386c2a142d94ff043
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2021-11-30 20:19:48 -0600

    Add files via upload

--- javascript/function.js
+++ javascript/function.js
@@ -1,32 +1,36 @@
 
-import { getDatabase, ref, onValue } from "firebase/database";
-const database = getDatabase();
-var ref = firebase.database().ref('Plantas');
-ref.orderByKey(), function(snapshot) {
-    console.log(snapshot.key);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAKyAjxAkpF9XBPF_pkvRO67bGCCEw8XaE",
+    authDomain: "dara-d66da.firebaseapp.com",
+    databaseURL: "https://dara-d66da-default-rtdb.firebaseio.com",
+    projectId: "dara-d66da",
+    storageBucket: "dara-d66da.appspot.com",
+    messagingSenderId: "710329915443",
+    appId: "1:710329915443:web:9e72382b8b22f5a4fbeb46"
 };
 
+const app = initializeApp(firebaseConfig);
 
-
-
-
-
-
-
-
-
-
-//Acceder a servicio de basesd de datos
-//const data = firebase.database();
-
-/*Obtener una referencia de la raiz de la base de datos
-let refToData = data.ref();
-
-//Obtener una console.log de todos los datos
-DataTransfer.once('value', snapshot => {
-    console.log(snapshot.val());
-})
- */
+const dbRef = ref(getDatabase(app));
+get(child(dbRef, `Registros /Sensor 1/Lectura`)).then((snapshot) => {
+    if (snapshot.exists()) {
+        let lect = snapshot.val();
+        const element = document.getElementById("lectura-id");
+        element.innerHTML = lect;
+        console.log(snapshot.val());
+    } else {
+        console.log("No data available");
+    }
+}).catch((error) => {
+    console.error(error);
+});
 
 
 
